fe66360c436812c1f768aeee32349edd0f41328d
James McCallum <jamesgmccallum@gmail.com>
2017-10-14 16:41:19 +1100

    Single partition suitable for growing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+# Add vagrant user to sudoers.
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
 # Installing vagrant keys
 mkdir ~/.ssh
 chmod 700 ~/.ssh
@@ -7,6 +11,51 @@ cd ~/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chown -R vagrant ~/.ssh
+
+# Create swap file
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# Install prl tools
+HOME_DIR="${HOME_DIR:-/home/vagrant}";
+
+case "$PACKER_BUILDER_TYPE" in
+    parallels-iso|parallels-pvm)
+        mkdir -p /tmp/parallels;
+        mount -o loop $HOME_DIR/prl-tools-lin.iso /tmp/parallels;
+        VER="`cat /tmp/parallels/version`";
+
+        echo "Parallels Tools Version: $VER";
+
+        /tmp/parallels/install --install-unattended-with-deps \
+            || (code="$?"; \
+                echo "Parallels tools installation exited $code, attempting" \
+                     "to output /var/log/parallels-tools-install.log"; \
+                cat /var/log/parallels-tools-install.log; \
+                exit $code);
+        umount /tmp/parallels;
+        rm -rf /tmp/parallels;
+        rm -f $HOME_DIR/*.iso;
+        ;;
+esac
+
+# Install virtualbox tools
+case "$PACKER_BUILDER_TYPE" in
+    virtualbox-iso|virtualbox-ovf)
+        VER="`cat /home/vagrant/.vbox_version`";
+        ISO="VBoxGuestAdditions_$VER.iso";
+        mkdir -p /tmp/vbox;
+        mount -o loop $HOME_DIR/$ISO /tmp/vbox;
+        sh /tmp/vbox/VBoxLinuxAdditions.run \
+            || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
+                    "For more read https://www.virtualbox.org/ticket/12479";
+        umount /tmp/vbox;
+        rm -rf /tmp/vbox;
+        rm -f $HOME_DIR/*.iso;
+        ;;
+esac
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
