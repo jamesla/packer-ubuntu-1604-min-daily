4716d28bc406da023b430e35e6bed9dcd74f9431
James McCallum <jamesgmccallum@gmail.com>
2017-10-14 18:13:18 +1100

    Remove zero filling step as not working anyway

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -58,9 +58,5 @@ case "$PACKER_BUILDER_TYPE" in
         ;;
 esac
 
-# Zero out the rest of the free space using dd, then delete the written file.
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-
-# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+# Add `sync` so Packer doesn't quit too early
 sync
